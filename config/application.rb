fceeb98cd57af83f6b5761fa901078ca88a52feb
yunu0705 <peche07yuta@gmail.com>
2024-11-04 15:48:05 +0900

    Update CORS settings

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,13 @@
+# config/application.rb
+
 require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Testapp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
     config.time_zone = 'Tokyo'
@@ -17,17 +16,12 @@ module Testapp
     # CORS設定の追加
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://test-app-peche-c2666ebb3dc5.herokuapp.com','https://alc-streamersland.com'
+        origins 'https://alc-streamersland.com'
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
           credentials: true  # 認証情報を含める場合にtrueを設定
       end
     end
-    
-
-
-    # その他の設定
-    config.paths.add 'lib', eager_load: true
   end
 end
